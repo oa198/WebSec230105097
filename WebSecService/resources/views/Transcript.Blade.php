80b3798081242f2a1d632dbbd18d3b119784ba2e
oa198 <oa148935@gmail.com>
2025-04-28 23:44:49 +0300

    edit in trans

--- WebSecService/resources/views/Transcript.Blade.php
+++ WebSecService/resources/views/Transcript.Blade.php
@@ -15,33 +15,42 @@
             <div class="card-body">
                 <h5><strong>Student Name:</strong> {{ $student['name'] }}</h5>
                 <h5><strong>Student ID:</strong> {{ $student['id'] }}</h5>
-                <h5><strong>Department:</strong> {{ $student['department'] }}</h5> 
+                <h5><strong>Department:</strong> {{ $student['department'] }}</h5>
             </div>
         </div>
 
-        <table class="table table-bordered table-striped text-center">
-            <thead class="table-dark">
-                <tr>
-                    <th>Course</th>
-                    <th>Credit Hours</th>
-                    <th>GPA</th>
-                    <th>Grade</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($student['courses'] as $course)
-                <tr>
-                    <td>{{ $course['name'] }}</td>
-                    <td>{{ $course['credit_hours'] }}</td>
-                    <td>{{ $course['gpa'] }}</td>
-                    <td>{{ $course['grade'] }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <div class="alert alert-warning text-center mt-3">
-            <h4><strong>Total GPA:</strong> {{ $finalGPA }}</h4>
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped text-center">
+                <caption class="text-start text-muted">Academic Performance</caption>
+                <thead class="table-dark">
+                    <tr>
+                        <th>Course</th>
+                        <th>Credit Hours</th>
+                        <th>GPA</th>
+                        <th>Grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($student['courses'] as $course)
+                    <tr>
+                        <td>{{ $course['name'] }}</td>
+                        <td>{{ $course['credit_hours'] }}</td>
+                        <td>{{ number_format($course['gpa'], 2) }}</td>
+                        <td>{{ $course['grade'] }}</td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="4">No courses available</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+                <tfoot>
+                    <tr class="table-warning fw-bold">
+                        <td colspan="3">Total GPA</td>
+                        <td>{{ number_format($finalGPA, 2) }}</td>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </div>
 </body>
--- WebSecService/resources/views/products.blade.php
+++ WebSecService/resources/views/products.blade.php
@@ -5,20 +5,30 @@
 @section('content')
 <div class="container mt-4">
     <h3 class="mb-3">Product Catalog</h3>
+
     <div class="row">
-        @foreach ($products as $product)
+        @forelse ($products as $product)
         <div class="col-md-4 mb-4">
-            <div class="card">
-            
-                <div class="card-body">
+            <div class="card h-100">
+                @if (!empty($product['image']))
+                    <img src="{{ $product['image'] }}" class="card-img-top" alt="{{ $product['name'] }}">
+                @endif
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">{{ $product['name'] }}</h5>
                     <p class="card-text">{{ $product['description'] }}</p>
                     <p class="card-text"><strong>${{ number_format($product['price'], 2) }}</strong></p>
-                    <button class="btn btn-primary">Add to Cart</button>
+                    <form action="{{ route('cart.add', $product['id']) }}" method="POST" class="mt-auto">
+                        @csrf
+                        <button type="submit" class="btn btn-primary w-100">Add to Cart</button>
+                    </form>
                 </div>
             </div>
         </div>
-        @endforeach
+        @empty
+        <div class="col-12">
+            <div class="alert alert-info">No products available at the moment. Please check back later.</div>
+        </div>
+        @endforelse
     </div>
 </div>
 @endsection
