dd677004e813f6ea3859b64484da0bdbf4b17ed2
oa198 <oa148935@gmail.com>
2025-04-28 23:48:04 +0300

    edit in mark

--- WebSecService/resources/views/exercises2/minitest.blade.php
+++ WebSecService/resources/views/exercises2/minitest.blade.php
@@ -10,34 +10,43 @@
         </div>
 
         <div class="card-body p-0">
-            <table class="table table-bordered mb-0">
-                <thead class="table-secondary">
-                    <tr>
-                        <th class="text-center">Item</th>
-                        <th class="text-center">Quantity</th>
-                        <th class="text-center">Price</th>
-                        <th class="text-center">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($marks as $item)
-                    <tr>
-                        <td>{{ $item['item'] }}</td>
-                        <td class="text-center">{{ $item['quantity'] }}</td>
-                        <td class="text-end">${{ number_format($item['price'], 2) }}</td>
-                        <td class="text-end">${{ number_format($item['quantity'] * $item['price'], 2) }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-                <tfoot class="table-light">
-                    <tr>
-                        <td colspan="3" class="text-end fw-bold">Total Amount</td>
-                        <td class="text-end fw-bold">
-                            ${{ number_format(collect($marks)->sum(fn($item) => $item['quantity'] * $item['price']), 2) }}
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
+            @if (count($items))
+            <div class="table-responsive">
+                <table class="table table-bordered mb-0">
+                    <caption class="text-muted">Summary of purchased items</caption>
+                    <thead class="table-secondary">
+                        <tr>
+                            <th class="text-center">Item</th>
+                            <th class="text-center">Quantity</th>
+                            <th class="text-center">Price</th>
+                            <th class="text-center">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($items as $item)
+                        <tr>
+                            <td>{{ $item['item'] }}</td>
+                            <td class="text-center">{{ $item['quantity'] }}</td>
+                            <td class="text-end">${{ number_format($item['price'], 2) }}</td>
+                            <td class="text-end">${{ number_format($item['quantity'] * $item['price'], 2) }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot class="table-light">
+                        <tr>
+                            <td colspan="3" class="text-end fw-bold">Total Amount</td>
+                            <td class="text-end fw-bold">
+                                ${{ number_format(collect($items)->sum(fn($item) => $item['quantity'] * $item['price']), 2) }}
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            @else
+            <div class="p-4 text-center text-muted">
+                No items found in the bill.
+            </div>
+            @endif
         </div>
     </div>
 </div>
